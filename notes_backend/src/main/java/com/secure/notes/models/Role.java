b611cc98c5d9be500b63bfe27a2b159d1989ceaf
Kalana98 <kalanamalshan98@gmail.com>
2025-05-03 12:07:11 +0530

    Security features updated: build custom user for the security

--- notes_backend/src/main/java/com/secure/notes/models/Role.java
+++ notes_backend/src/main/java/com/secure/notes/models/Role.java
@@ -6,7 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import org.springframework.security.core.userdetails.User;
 
 import java.util.HashSet;
 import java.util.Set;
